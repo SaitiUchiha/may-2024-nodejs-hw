0eb4c7b0655c1b59fc72a60b0aa4a1eba1f5db23
SaitiUchiha <vira.brods@gmail.com>
2024-12-05 17:33:44 +0200

    hw2

--- main.js
+++ main.js
@@ -9,17 +9,17 @@ const funct = async () => {
     for (const folderName of folders) {
         await fs.mkdir(path.join("baseFolder", folderName));
         await fs.writeFile('text.txt', 'Home Work 2',)
-       await fs.copyFile('text.txt', `${path.join("baseFolder", folderName, 'text.txt')}`);
+        await fs.copyFile('text.txt', `${path.join("baseFolder", folderName, 'text.txt')}`);
         await fs.unlink('text.txt');
         const directoryPathInquiry = path.join("baseFolder", folderName)
         console.log('Folder path', directoryPathInquiry)
         const stat = await fs.stat(directoryPathInquiry);
-        console.log(`${folderName} is Directory?`,stat.isDirectory());
-        console.log(`${folderName} is file?`,stat.isFile());
+        console.log(`${folderName} is Directory?`, stat.isDirectory());
+        console.log(`${folderName} is file?`, stat.isFile());
         const filePathInquiry = path.join("baseFolder", folderName, 'text.txt')
         const stat2 = await fs.stat(filePathInquiry);
-        console.log(`text.txt is Directory?`,stat2.isDirectory());
-        console.log(`text.txt is file?`,stat2.isFile());
+        console.log(`text.txt is Directory?`, stat2.isDirectory());
+        console.log(`text.txt is file?`, stat2.isFile());
     }
 
     // await fs.rm(path.join(process.cwd(), 'baseFolder'), { recursive: true, force: true });
